825d55a5be2518cb47eb7709c2e31bf0d232e25f
piguman3 <113848275+piguman3@users.noreply.github.com>
2025-10-22 16:10:43 -0300

    first oled test

--- runheadless.py
+++ runheadless.py
@@ -7,6 +7,16 @@ import time
 import keyboard
 import mouse
 
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import ssd1306
+from PIL import Image
+
+# rev.1 users set port=0
+serial = i2c(port=1, address=0x3C)
+
+device = ssd1306(serial, height=32)
+
 def on_keyboard_action(event):
     if event.event_type == keyboard.KEY_DOWN:
         pyautogui.keyDown(event.name)
@@ -25,8 +35,9 @@ def capture():
     while 1:
         img = ImageGrab.grab(xdisplay=":99")
         img = img.resize((128, 32), Image.Resampling.BICUBIC)
-        img.save(f"balls.png")
-        time.sleep(0.1)
+        with canvas(device) as draw:
+            flipped_im = img.transpose(Image.FLIP_LEFT_RIGHT)
+            draw.bitmap((0, 0), flipped_im, fill="white")
 
 disp = SmartDisplay(backend="xvfb", use_xauth=True, extra_args=[":99"], size=(320, 200))
 disp.start()
